e110f2bf19238cafca6843809ba990eb036b4fdf
balajigowri <balajidharmar95@gmail.com>
2023-02-11 14:35:27 +0530

    Array updated

--- Arraypractice.java
+++ Arraypractice.java
@@ -2,11 +2,20 @@ package javapractice;
 
 public class Arraypractice {
 public static void main(String[] args) {
-	int a [] = new int[10];
+	int a [] = new int[5];
 	a[1]=10;
 	a[2]=20;
 	a[3]=30;
 	a[4]=40;
-	System.out.println(a[1]);
-}
+	int length=a.length;
+	System.out.println(length);
+	
+	int a1[]= {100,200,300,400};
+	for (int i = 0; i < a1.length; i++) {
+		System.out.println(a1[i]);
+	}
+	
+	
+	
+	}
 }
